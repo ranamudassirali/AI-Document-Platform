190dfeab570d7253369027e74d22e41ee727b69d
Rana Mudassir Ali <ranamudassirali9@gmail.com>
2026-09-17 12:36:52 +0500

    Test CI failure

---
+++ tests/AIDocumentPlatform.UnitTests/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using AIDocumentPlatform.API.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AIDocumentPlatform.UnitTests;
+
+public class HealthControllerTests
+{
+    [Fact]
+    public void Get_ShouldReturnOkResult()
+    {
+        // Arrange
+        var controller = new HealthController();
+
+        // Act
+        var result = controller.Get();
+
+        // Assert
+        Assert.IsType<BadRequestObjectResult>(result);
+    }
+}
